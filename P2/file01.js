73779bed56ac6c456b625ab1b168e01899621a67
Arpan Godiyal <31252109+arpangodiyal@users.noreply.github.com>
2018-09-08 12:39:21 +0530

    Update file01.js

--- P2/file01.js
+++ P2/file01.js
@@ -1,23 +1,80 @@
 var budgetController = (function(){
 
-	x = 25;
-	function addition(){
-		return x + 5;
+	var Income = function(id, description, value){
+		this.id = id;
+		this.description = description;
+		this.value = value;
 	}
 
-	return {
-		y : addition(),
+	var Expense = function(id, description, value){
+		this.id = id;
+		this.description = description;
+		this.value = value;
 	}
+
+	
 
 })();
+
+
+
+
+
 
 var UIcontroller = (function(){
 
+	var DOMstrings = {
+		add__type : '.add__type',
+		add__description : '.add__description',
+		add__value : '.add__value',
+		add__btn : '.add__btn'
+	};
 
+	return {
+		getInput : function(){
+			return {
+				addType : document.querySelector(DOMstrings.add__type).value,
+				description : document.querySelector(DOMstrings.add__description).value,
+				value :  document.querySelector(DOMstrings.add__value).value,
+			};
+		},
+
+		getDOMstrings : function(){
+			return DOMstrings;
+		}
+	};
 })();
+
+
+
+
 
 var Controller = (function(budgetCtrl, UIctrl){
 
-	console.log(budgetCtrl.y);
+	var startEventListener = function(){
+		DOMstrings = UIctrl.getDOMstrings();
+
+		document.querySelector(DOMstrings.add__btn).addEventListener('click', function(){
+			console.log(UIctrl.getInput());
+		});
+
+		document.addEventListener('keypress', function(event){
+
+			if(event.keyCode == 13){
+				console.log(UIctrl.getInput());
+			}
+
+		});
+	}
+
+	return {
+		init : function(){
+			console.log('Application has started');
+			startEventListener();
+		}
+	};
 
 })(budgetController, UIcontroller);
+
+
+Controller.init();
